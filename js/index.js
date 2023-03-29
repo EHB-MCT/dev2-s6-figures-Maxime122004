e971ce1d63685fa3723278b5b781f9b3b6ce956c
Maxime Bastien <maxime.bastien@student.ehb.be>
2023-03-29 19:56:12 +0200

    TAAK KLAAR Maxime Bastien

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 "use strict";
-//import Figure from './Figure.js';
+
 import Circle from './Circle.js';
 import Rectangle from './Rectangle.js';
 import Square from './Square.js';
@@ -8,9 +8,7 @@ const form = {
     changeSelect(e) {
         document.getElementById('otherInputs').innerHTML = '';
 
-        console.log('change', e.target.value);
         if(e.target.value === 'rectangle'){
-            console.log("Rectangle selected");
             form.inputElementsWithLabel('width', 'width');
             form.inputElementsWithLabel('height', 'height');
         }
@@ -22,7 +20,6 @@ const form = {
         }
     },
     init() {
-        console.log('Form init!');
         document.getElementById('select').addEventListener('change', form.changeSelect);
         document.getElementById('form').addEventListener('submit', form.submitForm);
     },
@@ -34,32 +31,23 @@ const form = {
         document.getElementById('otherInputs').insertAdjacentHTML('beforeend', htmlString);
     },
     submitForm(e) {
-        //console.log('submitForm!');
         e.preventDefault();
         const type = document.getElementById('select').value;
-        //console.log(type);
 
         if(type === 'rectangle'){
             const width = document.getElementById('width').value;
             const height = document.getElementById('height').value;
             const rect = new Rectangle('Rectangle', width, height);
-            console.log(rect);
-            console.log(rect.htmlString);
             document.getElementById("results").insertAdjacentHTML('beforeend', rect.htmlString);
         } else if(type === 'circle'){
             const radius = document.getElementById('radius').value;
             const circ = new Circle('Circle', radius);
-            console.log(circ);
-            console.log(circ.htmlString);
             document.getElementById("results").insertAdjacentHTML('beforeend', circ.htmlString);
         } else if(type === 'square'){
             const size = document.getElementById('size').value;
             const squa = new Square('Square', size);
-            console.log(squa);
-            console.log(squa.htmlString);
             document.getElementById("results").insertAdjacentHTML('beforeend', squa.htmlString);
         }
-
     }
 };
 
