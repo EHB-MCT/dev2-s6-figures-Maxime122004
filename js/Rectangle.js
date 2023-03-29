ebd0c40e04c0cc186be4deb9346c7a4935a1bc78
Maxime Bastien <maxime.bastien@student.ehb.be>
2023-03-29 19:18:41 +0200

    figures

---
+++ js/Rectangle.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import Figure from "./Figure.js";
+
+export default class Rectangle extends Figure{
+    constructor(name, width, height){
+        super(name);
+        this.height = height;
+        this.width = width;
+    }
+    get area(){
+        return this.width * this.height;
+    }
+    get circumference(){
+        return (this.width * this.height) * 2;
+    }
+    get htmlString(){
+        return `<div class="result">
+        <div class="figure rectangle" style="width: ${this.width}px; height: ${this.height}px;"></div>
+        <div class="infoBox">
+          <h3>${this.name}</h3>
+          <dl>
+            <dt>Height</dt>
+            <dd>${this.height} px</dd>
+      
+            <dt>Width</dt>
+            <dd>${this.width} px</dd>
+      
+            <dt>Area</dt>
+            <dd>${this.area} px</dd>
+      
+            <dt>Circumference</dt>
+            <dd>${this.circumference} px</dd>
+          </dl>
+        </div>
+      </div>`;
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,20 +1,52 @@
 "use strict";
 //import Figure from './Figure.js';
-//import Circle from './Circle.js';
-//import Rectangle from './Rectangle.js';
-//import Square from './Square.js';
+import Circle from './Circle.js';
+import Rectangle from './Rectangle.js';
+import Square from './Square.js';
 
 const form = {
     changeSelect(e) {
+        document.getElementById('otherInputs').innerHTML = '';
 
+        console.log('change', e.target.value);
+        if(e.target.value === 'rectangle'){
+            console.log("Rectangle selected");
+            form.inputElementsWithLabel('width', 'width');
+            form.inputElementsWithLabel('height', 'height');
+        }
+        else if(e.target.value === 'circle'){
+            form.inputElementsWithLabel('radius', 'radius');
+        }
+        else if(e.target.value === 'square'){
+            form.inputElementsWithLabel('size', 'size');
+        }
     },
     init() {
         console.log('Form init!');
+        document.getElementById('select').addEventListener('change', form.changeSelect);
+        document.getElementById('form').addEventListener('submit', form.submitForm);
     },
     inputElementsWithLabel(label, id) {
-
+        let htmlString = 
+        `<label> ${label} 
+        <input id="${id}" type="number">
+        </label>`;
+        document.getElementById('otherInputs').insertAdjacentHTML('beforeend', htmlString);
     },
     submitForm(e) {
+        //console.log('submitForm!');
+        e.preventDefault();
+        const type = document.getElementById('select').value;
+        //console.log(type);
+
+        if(type === 'rectangle'){
+            const width = document.getElementById('width').value;
+            const height = document.getElementById('height').value;
+            const rect = new Rectangle('Rectangle', width, height);
+            console.log(rect);
+            console.log(rect.htmlString);
+            document.getElementById("result").insertAdjacentHTML('beforeend', rect);
+        }
 
     }
 };
